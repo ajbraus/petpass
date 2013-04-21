bb9169000fbe8f8a1503dbd7b1637f7a3199f5e1
ajbraus <ajbraus@gmail.com>
2013-04-21 12:00:07 -0500

    fixed sign in added dotenv

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,8 @@
+require 'dotenv'
+Dotenv.load
+
 Petpass::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
